02548851d4440987ce1be121fe8af2b06605a411
[dyad] <git@dyad.sh>
2026-01-05 10:53:20 +0530

    [dyad] add Dyad component tagger

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dyadComponentTagger from '@dyad-sh/react-vite-component-tagger';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [dyadComponentTagger(), react()],
 })
